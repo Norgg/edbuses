e285c5ee7f04b3d54db8dcd4ae90c0efc705a6de
John Turner <groups@norgg.org>
2009-06-21 14:05:13 +0100

    hitch controller working

--- fix_the_buses/app/controllers/hitch_controller.rb
+++ fix_the_buses/app/controllers/hitch_controller.rb
@@ -3,7 +3,10 @@ class HitchController < ApplicationController
   end
 
   def lookup
-    
+    @params_disp = params
+    @route_str = params[:hitch][:route]
+    @route = Route.find_by_number(@route_str)
+    @stops = @route.get_stops_by_address(params[:hitch][:from_location])[0..1]
+    @times_str = @stops.map{|stop| stop.get_times(@route_str)}
   end
-
 end
