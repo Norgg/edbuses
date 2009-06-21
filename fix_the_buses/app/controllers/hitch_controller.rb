462669229575d2961f11be34f1b5fb51ee42487a
John Turner <groups@norgg.org>
2009-06-21 14:40:57 +0100

    fixed some crashyness.

--- fix_the_buses/app/controllers/hitch_controller.rb
+++ fix_the_buses/app/controllers/hitch_controller.rb
@@ -3,10 +3,14 @@ class HitchController < ApplicationController
   end
 
   def lookup
-    @params_disp = params
-    @route_str = params[:hitch][:route]
-    @route = Route.find_by_number(@route_str)
-    @stops = @route.get_stops_by_address(params[:hitch][:from_location])[0..1]
-    @times_str = @stops.map{|stop| stop.get_times(@route_str)}
+    if params[:hitch]
+      @route_str = params[:hitch][:route]
+      @route = Route.find_by_number(@route_str)
+      if @route
+        @stops = @route.get_stops_by_address(params[:hitch][:from_location])
+        @stops = @stops[0..1] if @stops
+        @times_str = @stops.map{|stop| stop.get_times(@route_str)} if @stops
+      end
+    end
   end
 end
