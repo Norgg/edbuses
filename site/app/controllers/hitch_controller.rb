cc47e2dba8ec0acb89606778c3c991857372b75b
John Turner <groups@norgg.org>
2009-12-10 18:11:27 +0000

    Fixed routes up a bit.

--- site/app/controllers/hitch_controller.rb
+++ site/app/controllers/hitch_controller.rb
@@ -1,11 +1,8 @@
 class HitchController < ApplicationController
-  def index
-  end
-
   def lookup
-    if params[:hitch]
-      @stops = Stop.find_by_address(params[:hitch][:from_location], 2)
-      @place = params[:hitch][:from_location]
+    if params[:from]
+      @stops = Stop.find_by_address(params[:from], 2)
+      @place = params[:from]
     end
   end
 end
